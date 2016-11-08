a8bd0758c9fd0606e4b65eb6949b6c751e8bdecb
OldAndInYourWay <metroidman12@gmail.com>
2016-11-07 20:07:26 -0500

    Edited deploy script:

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,38 +1,42 @@
 #!bin/bash
 
 ssh ec2-user@10.0.1.155 <<EOF
-  install git
   sudo yum -y update
   sudo yum -y install gcc-c++ make
   sudo yum -y install openssl-devel
   sudo yum -y install git
 
   # install node
-  sudo yum install nodejs npm --enablerepo=epel
+  sudo yum -y install nodejs npm --enablerepo=epel
 
   # update npm
-  npm install -g npm
+  sudo npm install -g npm
+  sudo npm install forever --global
 
   git clone https://github.com/stemmlerjs/movie-quotes.git
   cd movie-quotes
-  npm run start
+  npm install && npm run start
+  done
 EOF
 
 ssh ec2-user@10.0.3.100 <<EOF
-  install git
-  sudo yum -y update
-  sudo yum -y install gcc-c++ make
-  sudo yum -y install openssl-devel
-  sudo yum -y install git
+  sudo su
+  yum -y update
+  yum -y install gcc-c++ make
+  yum -y install openssl-devel
+  yum -y install git
 
   # install node
-  sudo yum install nodejs npm --enablerepo=epel
+  yum -y install nodejs npm --enablerepo=epel
 
   # update npm
   npm install -g npm
+  npm install forever --global
 
   git clone https://github.com/stemmlerjs/movie-quotes.git
   cd movie-quotes
-  npm run start
+  npm install && npm run start
+  done
+  
 EOF
 
